7c7f08449c87f97626e306060e204ca835c35f6b
denchief1 <jacob@iharrand.com>
2020-10-21 15:47:45 -0400

    GenericRepositoryCreated

---
+++ DataAccess/DataAccessLibrary/DataAccessLibrary/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public class GenericRepository<T> : IGenericRespository<T> where T: class
+    {
+        private readonly DbContext _context;
+        private DbSet<T> table;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            table = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetByID(object Id)
+        {
+            return table.Find(Id);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate) 
+        {
+            return table.Where(predicate);
+        }
+
+        public void Add(T obj)
+        {
+            table.Add(obj);
+        }
+        
+        public void AddRange(IEnumerable<T> objs)
+        {
+            table.AddRange(objs);
+        }
+
+        public void Remove(T obj)
+        {
+            table.Remove(obj);
+        }
+
+        public void RemoveRange(IEnumerable<T> objs)
+        {
+            table.RemoveRange(objs);
+        }
+    }
+}
---
+++ DataAccess/DataAccessLibrary/DataAccessLibrary/IGenericRespository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public interface IGenericRespository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(object Id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        void Add(T obj);
+        void AddRange(IEnumerable<T> objs);
+
+        void Remove(T obj);
+        void RemoveRange(IEnumerable<T> objs);
+    }
+}
